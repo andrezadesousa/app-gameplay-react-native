7a8693ccef5942e4ca3ebea9a60a09126f562c53
Andreza Sousa <andrezanascimentodesousa@gmail.com>
2024-01-19 14:54:01 -0300

    Fix: created SignIn screen

--- src/screen/SignIn/index.tsx
+++ src/screen/SignIn/index.tsx
@@ -1,11 +1,34 @@
 import React from "react";
 import { styles } from "./styles";
-import { View, Text } from "react-native";
+import { View, Text, Image, StatusBar } from "react-native";
+
+import IllustrationImg from "../../assets/illustration.png";
+
+import { ButtonIcon } from "../../components/Buttonicon";
 
 export function SignIn() {
-  return(
+  return (
     <View style={styles.container}>
-        <Text>Hello world!</Text>
+      {/* Muda a cor da bar do celular */}
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+
+      <Image source={IllustrationImg} style={styles.image} resizeMode="stretch" />
+
+      <View style={styles.content}>
+        <Text style={styles.title}>
+          Organize {'\n'}
+          suas jogatinas {'\n'}
+          facilmente
+        </Text>
+
+        <Text style={styles.subtitle}>
+          Crie grupos para jogar seus games {'\n'}
+          favoritos com seus amigos
+        </Text>
+
+        <ButtonIcon />
+
+      </View>
     </View>
-  )
+  );
 }
