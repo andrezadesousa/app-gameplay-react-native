b17b2d852c7f68a54897c4ffc60f29c3af3b204c
Andreza Sousa <andrezanascimentodesousa@gmail.com>
2024-01-23 13:21:44 -0300

    Fix: add svg typing

---
+++ src/@types/svg.d.ts
@@ -0,0 +1,7 @@
+// * todo arquivo que termina com svg
+declare module "*.svg" {
+    import React from "react";
+    import { SvgProps } from "react-native-svg";
+    const content: React.FC<SvgProps>;
+    export default content;
+}
