419b9d69a6dbfe839006d768e722732d67d64af3
Andreza Sousa <andrezanascimentodesousa@gmail.com>
2024-01-23 13:22:04 -0300

    Fix: created svg categories

---
+++ src/utils/categories.ts
@@ -0,0 +1,12 @@
+// Definir quais são as categorias que a aplicação terá 
+import RankedSvg from '../assets/rankedSvg.svg';
+import DuelSvg from '../assets/duelSvg.svg';
+import FunSvg from '../assets/funSvg.svg';
+import TrainingSvg from '../assets/trainingSvg.svg';
+
+export const categories = [
+    {id: '1', title: 'Ranqueada', icon: RankedSvg},
+    {id: '2', title: 'Duelo 1x1', icon: DuelSvg},
+    {id: '3', title: 'Diversão', icon: FunSvg},
+    {id: '4', title: 'Treino', icon: TrainingSvg}
+]
