1499641676cdc9dc4a31c36d27940534af4b0f2b
Andreza Sousa <andrezanascimentodesousa@gmail.com>
2024-01-25 21:15:37 -0300

    Component: created Appointment component

--- src/components/Appointment/index.tsx
+++ src/components/Appointment/index.tsx
@@ -3,11 +3,18 @@ import { RectButton, RectButtonProps } from "react-native-gesture-handler";
 import { View, Text } from "react-native";
 
 import { styles } from "./styles";
+import { theme } from "../../global/styles/theme";
+import PlayerSvg from "../../assets/player.svg";
+import CalendarSvg from "../../assets/calendar.svg";
+
+import { GuildIcon } from "../GuildIcon";
+import { categories } from "../../utils/categories";
 
 export type GuildProps = {
-  id: string;
-  name: string;
-  icon: string | null;
+  id: string
+  name: string
+  icon: string | null
+  owner: boolean;
 };
 
 export type AppointmentProps = {
@@ -27,6 +34,11 @@ type Props = RectButton & {
 };
 
 export function Appointment({ data, ...rest }: Props) {
+  // const [category] = data.category.split(";");
+  const [category] = categories.filter((item) => item.id === data.category);
+  const { owner } = data.guild;
+  const { primary, on, secondary50, secondary70 } = theme.colors;
+
   return (
     <RectButton {...rest}>
       <View style={styles.container}>
@@ -34,6 +46,21 @@ export function Appointment({ data, ...rest }: Props) {
         <View style={styles.content}>
           <View style={styles.header}>
             <Text style={styles.title}>{data.guild.name}</Text>
+            <Text style={styles.category}>{category.title}</Text>
+          </View>
+
+          <View style={styles.footer}>
+            <View style={styles.dateInfo}>
+              <CalendarSvg />
+              <Text style={styles.date}>{data.date}</Text>
+            </View>
+
+            <View style={styles.playersInfo}>
+              <PlayerSvg fill={owner ? primary : on} />
+              <Text style={[styles.player, { color: owner ? primary : on }]}>
+                {owner ? "Anfitrião" : "Visitante"}
+              </Text>
+            </View>
           </View>
         </View>
       </View>
