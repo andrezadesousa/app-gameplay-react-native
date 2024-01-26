a5d01ee320f50c32a13bea5c06dcdd26357af721
Andreza Sousa <andrezanascimentodesousa@gmail.com>
2024-01-25 21:30:16 -0300

    Fix: change on home screen

--- src/screen/Home/index.tsx
+++ src/screen/Home/index.tsx
@@ -1,18 +1,20 @@
 import React, { useState } from "react";
-import { View } from "react-native";
+import { View, FlatList } from "react-native";
 
 import { styles } from "./styles";
 
 import { Profile } from "../../components/Profile";
 import { ButtonAdd } from "../../components/ButtonAdd";
 import { CategorySelect } from "../../components/CategorySelect";
+import { ListHeader } from "../../components/ListHeader";
+import { Appointment } from "../../components/Appointment";
+import { ListDivider } from "../../components/ListDivider";
 
 export function Home() {
   const [category, setCategory] = useState("");
 
   function handleCategorySelect(categoryId: string) {
     categoryId === category ? setCategory("") : setCategory(categoryId);
-
     // outra forma
     // if(categoryId === category){
     //   console.log(categoryId)
@@ -22,6 +24,22 @@ export function Home() {
     // }
   }
 
+  const appointments = [
+    {
+      id: "1",
+      guild: {
+        id: "1",
+        name: "Lendários",
+        icon: null,
+        owner: true,
+      },
+      category: "1",
+      date: "22/06 às 20:40h",
+      description:
+        "É hoje que vamos chegar ao challenger sem perder uma partida da md10",
+    },
+  ];
+
   return (
     <View>
       <View style={styles.header}>
@@ -29,10 +47,20 @@ export function Home() {
         <ButtonAdd />
       </View>
 
-      <View>
-        <CategorySelect
-          categorySelected={category}
-          setCategory={handleCategorySelect}
+      <CategorySelect
+        categorySelected={category}
+        setCategory={handleCategorySelect}
+      />
+
+      <View style={styles.content}>
+        <ListHeader title="Partidas agendadas" subtitle="Total 6" />
+        <FlatList
+          data={appointments}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <Appointment data={item} />}
+          ItemSeparatorComponent={() => <ListDivider />}
+          style={styles.matches}
+          showsVerticalScrollIndicator={false}
         />
       </View>
     </View>
--- src/screen/Home/styles.ts
+++ src/screen/Home/styles.ts
@@ -3,6 +3,9 @@ import { getStatusBarHeight } from "react-native-iphone-x-helper";
 import { theme } from "../../global/styles/theme";
 
 export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
   header: {
     width: "100%",
     paddingHorizontal: 24,
@@ -11,4 +14,12 @@ export const styles = StyleSheet.create({
     marginTop: getStatusBarHeight() + 26,
     marginBottom: 42,
   },
+  content: {
+    marginTop: 42,
+  },
+  matches: {
+    marginTop: 24,
+    marginLeft: 24,
+  }
 });
+ 
