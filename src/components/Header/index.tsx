10c71762e4adfe5cfc4ee98123d902b277849283
Andreza Sousa <andrezanascimentodesousa@gmail.com>
2024-01-29 13:43:48 -0300

    Fix: created Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,17 +7,23 @@ import { LinearGradient } from "expo-linear-gradient";
 
 import { styles } from "./styles";
 import { theme } from "../../global/styles/theme";
+import { useNavigation } from "@react-navigation/native";
 
 type Props = {
   title: string;
   action?: ReactNode; //essa acção será um nó do react
 };
 
-export function Avatar({ title, action }: Props) {
+export function Header({ title, action }: Props) {
   const { secondary100, secondary40, heading } = theme.colors;
+  const navigation = useNavigation();
+
+  function handleGoBack() {
+    navigation.goBack();
+  }
   return (
     <LinearGradient colors={[secondary100, secondary40]} style={styles.container}>
-      <BorderlessButton>
+      <BorderlessButton onPress={handleGoBack}>
         <Feather name="arrow-left" size={24} color={heading} /> 
       </BorderlessButton>
       <Text style={styles.title}> {title} </Text>
