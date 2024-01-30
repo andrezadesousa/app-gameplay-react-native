dc7bb3d8dd859969aa49ef1f79e4fc22e4f58c74
Andreza Sousa <andrezanascimentodesousa@gmail.com>
2024-01-30 17:59:07 -0300

    Fix: created SmallInput component

--- src/components/SmallInput/index.tsx
+++ src/components/SmallInput/index.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { TextInput, TextInputProps } from "react-native";
 
 import { styles } from "./styles";
-import { theme } from "../../global/styles/theme";
 export function SmallInput({...rest}: TextInputProps) {
   return (
     <TextInput 
